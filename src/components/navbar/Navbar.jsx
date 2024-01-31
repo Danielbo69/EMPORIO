d583e41206b196ba3a83b9f16fb82457e7beaeab
Daniel Boscan <danielboscanramos@gmail.com>
2024-01-31 11:38:18 -0300

    logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Offcanvas from "react-bootstrap/Offcanvas";
-import ImgLogo from "../../assets/img/LOGO.png"
+import ImgLogo from "../../assets/img/logo.png"
 
 function NavMenu() {
   const navigate = useNavigate();
